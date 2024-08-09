53a6a7daad2fde5b8104bed3074c4ec7f0f370dd
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-09 11:20:23 +0300

    docs(infrast): add godoc for jwt service

--- infrastructure/jwt/service.go
+++ infrastructure/jwt/service.go
@@ -1,3 +1,19 @@
+/*
+Package jwt provides functionality for creating and validating JSON Web Tokens (JWTs).
+It includes methods for generating tokens with user claims and decoding tokens to
+extract claims.
+
+Key Components:
+  - Service: Implements JWT operations including token generation and validation.
+  - Config: Holds the configuration for creating a new JWT Service.
+  - New: Creates a new Service instance with the given configuration.
+  - Generate: Generates a JWT for a given user with specified claims.
+  - Decode: Decodes and validates a JWT, returning the claims if valid.
+
+Dependencies:
+- github.com/dgrijalva/jwt-go: Library for working with JWTs.
+- github.com/beka-birhanu/domain/models/user: User model for JWT claims.
+*/
 package jwt
 
 import (
@@ -8,20 +24,21 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-// Service implements the ijwt.IService interface for handling JWT operations.
+// Service handles JWT operations.
 type Service struct {
 	secretKey string
 	issuer    string
 	expTime   time.Duration
 }
 
-// Config holds the configuration for creating a new JWT Service.
+// Config holds JWT service configuration.
 type Config struct {
 	SecretKey string
 	Issuer    string
 	ExpTime   time.Duration
 }
 
+// New creates a new JWT Service with the provided configuration.
 func New(config Config) *Service {
 	return &Service{
 		secretKey: config.SecretKey,
@@ -30,6 +47,7 @@ func New(config Config) *Service {
 	}
 }
 
+// Generate creates a JWT for the given user.
 func (s *Service) Generate(user *usermodel.User) (string, error) {
 	expirationTime := time.Now().UTC().Add(s.expTime).Unix()
 	claims := jwt.MapClaims{
@@ -43,6 +61,7 @@ func (s *Service) Generate(user *usermodel.User) (string, error) {
 	return token.SignedString([]byte(s.secretKey))
 }
 
+// Decode parses and validates a JWT, returning the claims if valid.
 func (s *Service) Decode(tokenString string) (jwt.MapClaims, error) {
 	token, err := jwt.Parse(tokenString, s.getSigningKey)
 	if err != nil {
@@ -56,9 +75,11 @@ func (s *Service) Decode(tokenString string) (jwt.MapClaims, error) {
 	return nil, errors.New("invalid token")
 }
 
+// getSigningKey returns the signing key for token validation.
 func (s *Service) getSigningKey(token *jwt.Token) (interface{}, error) {
 	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 		return nil, errors.New("unexpected signing method")
 	}
 	return []byte(s.secretKey), nil
 }
+
