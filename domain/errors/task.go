702f975691ecb081a65407a7a0226613d14a5fde
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-09 10:51:44 +0300

    docs(domain-errors): update godoc for task errors/task

--- domain/errors/task.go
+++ domain/errors/task.go
@@ -2,8 +2,16 @@ package err
 
 // Validation errors
 var (
-	ErrTitleEmpty       = NewValidation("title cannot be empty")
+	// ErrTitleEmpty indicates that the title cannot be empty.
+	ErrTitleEmpty = NewValidation("title cannot be empty")
+
+	// ErrDescriptionEmpty indicates that the description cannot be empty.
 	ErrDescriptionEmpty = NewValidation("description cannot be empty")
-	ErrDueDateZero      = NewValidation("due date cannot be zero")
-	ErrInvalidStatus    = NewValidation("invalid status")
+
+	// ErrDueDateZero indicates that the due date cannot be zero.
+	ErrDueDateZero = NewValidation("due date cannot be zero")
+
+	// ErrInvalidStatus indicates that the status is invalid.
+	ErrInvalidStatus = NewValidation("invalid status")
 )
+
