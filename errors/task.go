3b11c3ab44410fbf05b9a6da9e4d6ecba5355074
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-09 10:41:21 +0300

    refactor: restructure errors/task.go to align with clean architecture principles

--- errors/task.go
+++
@@ -1,9 +0,0 @@
-package err
-
-// Validation errors
-var (
-	ErrTitleEmpty       = NewValidation("title cannot be empty")
-	ErrDescriptionEmpty = NewValidation("description cannot be empty")
-	ErrDueDateZero      = NewValidation("due date cannot be zero")
-	ErrInvalidStatus    = NewValidation("invalid status")
-)
