4e4e851b171ac31fc56ac92b527ae161e0924f8a
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-10 14:45:59 +0300

    feat(api): add base controller to provide global error handling
    - The primary usage is to remove unwanted error message form liking to
    end users.

---
+++ api/controllers/base/controller.go
@@ -0,0 +1,54 @@
+package basecontroller
+
+import (
+	"net/http"
+
+	errapi "github.com/beka-birhanu/api/errors"
+	"github.com/gin-gonic/gin"
+)
+
+// BaseHandler is a base struct for all HTTP request handlers that provides basic HTTP functionalities.
+type BaseHandler struct{}
+
+// Problem handles errors by writing an appropriate response to the Gin context.
+// The primary usage is to hid some messages.
+func (h *BaseHandler) Problem(c *gin.Context, err errapi.Error) {
+	var shadowedErr errapi.Error
+	switch err.StatusCode() {
+	case errapi.BadRequest, errapi.Conflict, errapi.NotFound, errapi.Forbidden:
+		shadowedErr = err
+	case errapi.Authentication:
+		shadowedErr = errapi.NewAuthentication("invalid credentials")
+	default:
+		shadowedErr = errapi.NewServerError("something went wrong")
+	}
+	h.respondError(c, shadowedErr)
+}
+
+// RespondError writes an error response to the Gin context.
+func (h *BaseHandler) respondError(c *gin.Context, err errapi.Error) {
+	c.JSON(err.StatusCode(), gin.H{"error": err.Error()})
+}
+
+// RespondWithCookies writes a response with cookies to the Gin context.
+func (h *BaseHandler) RespondWithCookies(c *gin.Context, status int, v interface{}, cookies []*http.Cookie) {
+	for _, cookie := range cookies {
+		http.SetCookie(c.Writer, cookie)
+	}
+	h.Respond(c, status, v)
+}
+
+// RespondWithLocation writes a response with a Location header to the Gin context.
+func (h *BaseHandler) RespondWithLocation(c *gin.Context, status int, v interface{}, resourceLocation string) {
+	c.Header("Location", resourceLocation)
+	h.Respond(c, status, v)
+}
+
+// Respond writes a JSON response to the Gin context.
+func (h *BaseHandler) Respond(c *gin.Context, status int, v interface{}) {
+	if v == nil {
+		c.Status(status)
+	} else {
+		c.JSON(status, v)
+	}
+}
